348b68888afafb9708c602a64e884d634cd33f57
Navneet-Tricon <navneet.kumar@triconinfotech.com>
2024-12-05 12:49:19 +0530

    updating feature1 and feature2 into the feature branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,19 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderWrapperComponent } from "./header-wrapper/header-wrapper.component";
-import {SearchProductComponent} from "./search-product/search-product.component";
-import {ImageGalleryComponent} from "./image-gallery/image-gallery.component";
-import { A11yModule } from '@angular/cdk/a11y';
+
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    FooterComponent,
     HeaderWrapperComponent,
-    SearchProductComponent,
-    ImageGalleryComponent,
-    A11yModule,
+    FooterComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
 import { SidenavComponent } from "../sidenav/sidenav.component";
+import { FooterComponent } from '../footer/footer.component';
+import { HeaderWrapperComponent } from '../header-wrapper/header-wrapper.component';
+import { SearchProductComponent } from '../search-product/search-product.component';
+import { ImageGalleryComponent } from '../image-gallery/image-gallery.component';
+import { A11yModule } from '@angular/cdk/a11y';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    SidenavComponent
+    SidenavComponent,
+    FooterComponent,
+    HeaderWrapperComponent,
+    SearchProductComponent,
+    ImageGalleryComponent,
+    A11yModule,
+  
 ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
