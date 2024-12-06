b7d45c936bf5691e3572944476df107b39e05fa0
Sachin Biradar <Sachin.Biradar@Triconinfotech.com>
2024-12-06 12:12:30 +0530

    Added the focus trap feature and breadcrumb feature

--- src/app/findcontent/findcontent.component.ts
+++ src/app/findcontent/findcontent.component.ts
@@ -1,9 +1,6 @@
-import { A11yModule } from '@angular/cdk/a11y';
 import { CommonModule } from '@angular/common';
 import { Component} from '@angular/core';
 import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
-import { BreadcrumbComponent } from '../breadcrumb/breadcrumb.component';
-import { TrapFocusDirective } from '../trap-focus.directive';
 
 @Component({
   selector: 'app-findcontent',
@@ -11,9 +8,6 @@ import { TrapFocusDirective } from '../trap-focus.directive';
   imports: [
     NgbDropdownModule,
     CommonModule,
-    A11yModule,
-    BreadcrumbComponent,
-    TrapFocusDirective,
   ],
   templateUrl: './findcontent.component.html',
   styleUrl: './findcontent.component.scss',
