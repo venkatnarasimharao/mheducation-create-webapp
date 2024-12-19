4e953e0758e589bd1c78402454841483bebeb8a6
Sachin Biradar <Sachin.Biradar@Triconinfotech.com>
2024-12-19 20:24:45 +0530

    'updatedchanges'

--- src/app/shared/components/breadcrumb/breadcrumb.component.ts
+++ src/app/shared/components/breadcrumb/breadcrumb.component.ts
@@ -1,27 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'hec-breadcrumb',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './breadcrumb.component.html',
   styleUrl: './breadcrumb.component.scss',
 })
 export class BreadcrumbComponent {
-  @Input() list: string[] = [
-    'Find Content',
-    'Browse Collection',
-    'Negotiation Collection',
+  @Input() list: any[] = [
+    { label: 'Find Content', routerLink: '/findcontent' },
+    { label: 'Browse Collections', routerLink: '/browseCollection' },
+    { label: 'Negotiation Collection', routerLink: '/negotiationCollection' },
   ];
-
-    // checking last breadcrumb item
-    isLastItem(index: number): boolean {
-      return index === this.list.length - 1;
-    }
-  
-    // Method to get the class for a breadcrumb item
-    getClassForItem(index: number): string {
-      return this.isLastItem(index) ? 'inactive' : 'active';
-    }
 }
