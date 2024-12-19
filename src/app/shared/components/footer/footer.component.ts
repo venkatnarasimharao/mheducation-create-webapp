1e8fed2a0330425e7698a4435c9255ab7a4252d1
Anish Kumar <anish_k@ce.iitr.ac.in>
2024-12-19 17:58:38 +0530

    calling api for language and use session storage for persisting language change

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, TemplateRef } from '@angular/core';
+import { Component, inject, OnInit, TemplateRef } from '@angular/core';
 import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { SharedstateService } from '../../../core/services/shared-state/sharedstate.service';
 
 @Component({
   selector: 'footer',
@@ -10,23 +11,44 @@ import { TranslateModule, TranslateService } from '@ngx-translate/core';
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+
+  constructor(private sharedstateService: SharedstateService) { }
+  languages: { value: string, label: string, dir: string }[] = [];
+  ngOnInit(): void {
+    const savedLanguage = sessionStorage.getItem('selectedLanguage');
+    if (savedLanguage) {
+      this.selectedLanguage = savedLanguage;
+    }
+    this.sharedstateService.getLanguages();
+    const languagesSignal = this.sharedstateService.getLanguagesSignal();
+    if (languagesSignal()) {
+      const language = languagesSignal();
+      console.log(language);
+      if (language && Array.isArray(language)) {
+        this.languages = language.map((lang: any) => {
+          return {
+            value: lang.name._text,
+            label: lang.displayValue._text,
+            dir: lang.direction._text
+          }
+        })
+      }
+    }
+    console.log(this.selectedLanguage);
+  }
 
   translate: TranslateService = inject(TranslateService);
 
   private modalService = inject(NgbModal);
   closeResult = '';
   selectedLanguage: string = "English";
-  languages: string[] = [
-    'English', '简体中文', '繁體中文', 'العربية', '한국어',
-    'Italiano', 'Bahasa Malaysia', 'ภาษาไทย', '日本語', 'Català',
-    'Español (Castellano)', 'Español latinoamericano'
-  ];
 
   minRows: number = 5;
+  // TODO  add api-language type in interface
 
-  getLanguageColumns(): string[][] {
-    const result: string[][] = [];
+  getLanguageColumns(): { value: string; label: string; dir: string }[][] {
+    const result: { value: string; label: string; dir: string }[][] = [];
     const chunkSize = 5;
 
     for (let i = 0; i < this.languages.length; i += chunkSize) {
@@ -35,8 +57,9 @@ export class FooterComponent {
 
     return result;
   }
-  selectLanguage(language: string): void {
-    this.selectedLanguage = language;
+  selectLanguage(language: { value: string; label: string; dir: string }): void {
+    this.selectedLanguage = language.value;
+    sessionStorage.setItem('selectedLanguage', this.selectedLanguage);
 
   }
 
@@ -63,3 +86,4 @@ export class FooterComponent {
   }
 
 }
+
