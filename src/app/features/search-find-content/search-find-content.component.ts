e560442eef2f8ee40a150cab9ba163024d1f52fa
Sachin Biradar <Sachin.Biradar@Triconinfotech.com>
2024-12-20 19:09:03 +0530

    'updatedNamingconvention'

--- src/app/features/search-find-content/search-find-content.component.ts
+++ src/app/features/search-find-content/search-find-content.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { CustomdropdownComponent } from '../../shared/components/customdropdown/customdropdown.component';
 import { BreadcrumbComponent } from '../../shared/components/breadcrumb/breadcrumb.component';
 import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 import { CommonModule } from '@angular/common';
@@ -9,7 +8,6 @@ import { NgbDropdownToggleNoCaretDirective } from '../../shared/directives/dropd
   selector: 'hec-search-find-content',
   standalone: true,
   imports: [
-    CustomdropdownComponent,
     BreadcrumbComponent,
     NgbDropdownModule,
     CommonModule,
@@ -20,24 +18,24 @@ import { NgbDropdownToggleNoCaretDirective } from '../../shared/directives/dropd
 })
 export class SearchFindContentComponent {
   //dropdownTitle
-  firstDropDownTitle: string = 'Test123';
-  secondDropDownTitle: string = 'Please Select';
-  thirdDropdownTitle: string = 'Arrange';
+  selectProjectTitle: string = 'Test123';
+  selectFormatTitle: string = 'Please Select';
+  selectArrangeTitle: string = 'Arrange';
 
   //dropdown items
-  firstDropdownItems = ['Option 1', 'Option 2', 'Option 3'];
-  secondDropdownItems = ['Item A', 'Item B', 'Item C'];
+  selectProjectItems = ['Option 1', 'Option 2', 'Option 3'];
+  selectFormatItems = ['Item A', 'Item B', 'Item C'];
 
   //dropdown heading
-  firstDropdownHeading: string = 'Select Project';
-  secondDropdownHeading: string = 'Select Format';
-  thirdDropdownHeading: string = '86 pgs / $12.46 est';
+  selectProjectHeading: string = 'Select Project';
+  selectFormatHeading: string = 'Select Format';
+  arrangeHeading: string = '86 pgs / $12.46 est';
 
   onSelect(item: string) {
-    this.firstDropDownTitle = item; 
+    this.selectProjectTitle = item; 
   }
 
   onSelected(item: string) {
-    this.secondDropDownTitle = item; 
+    this.selectFormatTitle = item; 
   }
 }
