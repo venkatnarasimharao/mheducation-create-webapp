651dbeffed8782a3b7d88d03b1ad5ba3068d8f48
Venkat Narasimha Rao G <VenkataNarasimha.Rao@Triconinfotech.com>
2024-12-24 15:14:21 +0530

    feat: xml json transformer util

--- src/app/core/interceptors/http.interceptor.ts
+++ src/app/core/interceptors/http.interceptor.ts
@@ -1,43 +1,47 @@
-import { HttpErrorResponse, HttpHeaders, HttpInterceptorFn } from '@angular/common/http';
-import { catchError, throwError } from 'rxjs';
+import { HttpErrorResponse, HttpInterceptorFn, HttpResponse } from '@angular/common/http';
+import { catchError, map, throwError } from 'rxjs';
+import { XmlTransformerUtil } from '../../shared/utils/xml-transformer/xml-transformer.util';
 
 export const httpInterceptor: HttpInterceptorFn = (request, next) => {
   const token = sessionStorage.getItem('token');
-  if (!token) {
-    return next(request);
+
+  let transformedReq = token
+    ? request.clone({
+      headers: request.headers.set('Authorization', `Bearer ${token}`),
+    })
+    : request;
+
+  if (request.body && typeof request.body === 'object') {
+    const xmlBody = XmlTransformerUtil.jsonToXml(request.body);
+    transformedReq = request.clone({
+      body: xmlBody,
+      headers: request.headers.set('Content-Type', 'application/xml'),
+    });
   }
 
-  let req = request.clone({
-    headers: new HttpHeaders({
-      'Authorization': `Bearer ${token}`,
-    })
-  })
-
-  return next(req).pipe(
+  return next(transformedReq).pipe(
+    map((event: any) => {
+      if (event instanceof HttpResponse && event.headers?.get('Content-Type')?.includes('xml')) {
+        const xmlString = event.body as string;
+        const jsonResponse = XmlTransformerUtil.xmlToJson(xmlString);
+        event = event.clone({ body: JSON.stringify(jsonResponse) });
+      }
+      return event;
+    }),
     catchError((error: any) => {
-      console.error(error);
+      console.error('HTTP Error:', error);
+
       if (error instanceof HttpErrorResponse) {
         if (error.error instanceof ErrorEvent) {
-          console.error("Error Event");
+          console.error('Client-side Error:', error.error.message);
         } else {
-          console.log(`error status : ${error.status} ${error.statusText}`);
-          switch (error.status) {
-            case 401: {
-              window.location.href = '/'
-              sessionStorage.clear();
-              break;
-            }
-            case 403: {
-              sessionStorage.clear();
-              window.location.href = '/'
-              break;
-            }
-          }
+          console.error(`Server-side Error: ${error.status} - ${error.statusText}`);
         }
       } else {
-        console.error("some thing else happened");
+        console.error('Unknown Error:', error);
       }
-      return throwError(error)
+
+      return throwError(() => error);
     })
-  )
+  );
 };
