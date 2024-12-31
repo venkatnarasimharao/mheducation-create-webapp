8bef63d89e72e181ca81f4de88003ea4714ac0ed
Venkat Narasimha Rao G <VenkataNarasimha.Rao@Triconinfotech.com>
2024-12-31 12:36:05 +0530

    landing component, xml transformer util test cases done

--- src/app/features/landing/landing.component.spec.ts
+++ src/app/features/landing/landing.component.spec.ts
@@ -6,6 +6,7 @@ import { TranslateModule } from '@ngx-translate/core';
 describe('LandingComponent', () => {
   let component: LandingComponent;
   let fixture: ComponentFixture<LandingComponent>;
+  let consoleLogSpy: jasmine.Spy;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -19,9 +20,19 @@ describe('LandingComponent', () => {
     fixture = TestBed.createComponent(LandingComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    consoleLogSpy = spyOn(console, 'log');
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call handleSearch and log the event', () => {
+    const mockEvent = {
+      categories: ['category1', 'category2'],
+      term: 'search term'
+    };
+    component.handleSearch(mockEvent);
+    expect(consoleLogSpy).toHaveBeenCalledWith('Search Data:', mockEvent);
   });
 });
--- src/app/shared/utils/xml-transformer/xml-transformer.util.spec.ts
+++ src/app/shared/utils/xml-transformer/xml-transformer.util.spec.ts
@@ -1,7 +1,109 @@
 import { XmlTransformerUtil } from './xml-transformer.util';
 
 describe('XmlTransformerUtil', () => {
-  it('should create an instance', () => {
-    expect(new XmlTransformerUtil()).toBeTruthy();
+
+  describe('xmlToJson', () => {
+    it('should convert a valid XML string to JSON', () => {
+      const xmlString = `
+        <person>
+          <name>John Doe</name>
+          <age>30</age>
+          <address>
+            <street>Main St</street>
+            <city>New York</city>
+          </address>
+        </person>`;
+      const result = XmlTransformerUtil.xmlToJson(xmlString);
+      expect(result).toBeDefined();
+    })
+
+    it('should handle an empty XML string', () => {
+      const xmlString = '';
+      const result = XmlTransformerUtil.xmlToJson(xmlString);
+      expect(result).toBeDefined();
+    });
+
+    it('should handle malformed XML string gracefully', () => {
+      const malformedXml = '<person><name>John Doe</name><age>30<address></person>';
+      const result = XmlTransformerUtil.xmlToJson(malformedXml);
+      expect(result).toBeDefined();
+    })
+  })
+
+  describe('jsonToXml', () => {
+    it('should convert a valid JSON object to XML string', () => {
+      const jsonObj = {
+        person: {
+          name: 'John Doe',
+          age: '30',
+          address: {
+            street: 'Main St',
+            city: 'New York'
+          }
+        }
+      }
+      const result = XmlTransformerUtil.jsonToXml(jsonObj);
+      expect(result).toBeDefined();
+    });
+
+    it('should handle empty JSON object', () => {
+      const jsonObj = {};
+      const result = XmlTransformerUtil.jsonToXml(jsonObj);
+      expect(result).toBeDefined();
+    });
+
+    it('should handle JSON with attributes', () => {
+      const jsonObj = {
+        person: {
+          _id: '123',
+          name: 'John Doe',
+          age: '30'
+        }
+      };
+      const result = XmlTransformerUtil.jsonToXml(jsonObj);
+      expect(result).toBeDefined();
+    });
+  });
+
+  describe('private methods', () => {
+    it('should handle contentNode method correctly', () => {
+      const xmlString = `
+        <root>
+          <content>
+            <text>This is a content node</text>
+          </content>
+        </root>`;
+      const parser = new DOMParser();
+      const xmlDoc = parser.parseFromString(xmlString, 'application/xml');
+      const rootNode = xmlDoc.documentElement;
+      const result = XmlTransformerUtil.xmlToJson(xmlString);
+      expect(result).toBeDefined();
+    });
+
+    it('should handle root method correctly', () => {
+      const xmlString = `
+        <person>
+          <name>John Doe</name>
+          <age>30</age>
+        </person>`;
+      const parser = new DOMParser();
+      const xmlDoc = parser.parseFromString(xmlString, 'application/xml');
+      const rootNode = xmlDoc.documentElement;
+      const result = XmlTransformerUtil.xmlToJson(xmlString);
+      expect(result).toBeDefined();
+    });
+  });
+
+  describe('Edge cases', () => {
+    it('should handle XML with no children correctly', () => {
+      const xmlString = '<root></root>';
+      const result = XmlTransformerUtil.xmlToJson(xmlString);
+      expect(result).toBeDefined();
+    });
+    it('should handle XML with only text nodes correctly', () => {
+      const xmlString = '<description>This is a description</description>';
+      const result = XmlTransformerUtil.xmlToJson(xmlString);
+      expect(result).toBeDefined();
+    });
   });
 });
